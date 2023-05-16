a638e938ec7e5f46a074d248a1a5cddd3c7a204d
Taka <103984069+Taka0007@users.noreply.github.com>
2023-05-16 14:38:10 +0900

    Update newton.py

--- root finding/newton.py
+++ root finding/newton.py
@@ -1,1 +1,15 @@
-temp
+# 実際に使用する際には方程式の部分を書き換えておく
+# また収束性にも注意が必要
+def f(x):
+    return 2*(x**3)-3*(x**2)-12*x+5
+    
+def df(x):
+    return 6*(x**2)-6*x-12
+    
+error = float(input())
+ans = 4.0 #初期値設定
+
+while abs(f(ans))>error:
+    ans = ans - f(ans)/df(ans)
+    
+print(ans)
